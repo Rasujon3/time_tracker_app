0d36a1aea946aaf7724278d53e7aa158ad017ee0
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-04-25 21:06:36 +0600

    Error handling with trycatch(End of 129)
    Error handling with trycatch(End of 129)

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -12,8 +12,12 @@ import 'package:firebase_auth/firebase_auth.dart';
 class SignInPage extends StatelessWidget {
 
   Future<void> _signInAnonymously() async {
+    try {
     final authResult = await FirebaseAuth.instance.signInAnonymously();
     print('${authResult.user.uid}');
+    } catch (e){
+      print(e.toString());
+    }
   }
 
   @override
