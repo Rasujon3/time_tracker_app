b5b271c897a5638358d67d46ab32c32566b7a00d
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-04-25 23:48:27 +0600

    138 Creating the home page
    138 Creating the home page

--- lib/apps/landing_page.dart
+++ lib/apps/landing_page.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:time_tracker_app/app/sign_in/sign_in_page.dart';
+import 'package:time_tracker_app/apps/home_page.dart';
 // import 'package:time_tracker_flutter_course/app/sign_in/sign_in_page.dart';
 
 class LandingPage extends StatefulWidget {
@@ -26,7 +27,7 @@ class _LandingPageState extends State<LandingPage> {
     );
     }
     else{
-      return Container(); // temporary placeholder for HomePage
+      return HomePage();
     }
   }
 }
