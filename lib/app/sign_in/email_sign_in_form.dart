daadd48f49095ffc2ea4ce1c43873e62f14e798e
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-04-27 13:49:37 +0600

    188 Adding the submit buttons
    188 Adding the submit buttons

--- lib/app/sign_in/email_sign_in_form.dart
+++ lib/app/sign_in/email_sign_in_form.dart
@@ -1,8 +1,6 @@
-
 import 'package:flutter/material.dart';
 
 class EmailSignInForm extends StatelessWidget {
-
   List<Widget> _buildChildren() {
     return [
       TextField(
@@ -11,11 +9,22 @@ class EmailSignInForm extends StatelessWidget {
           hintText: 'test@test.com',
         ),
       ),
+      SizedBox(height: 8.0),
       TextField(
         decoration: InputDecoration(
           labelText: 'Password',
         ),
         obscureText: true,
+      ),
+      SizedBox(height: 8.0),
+      RaisedButton(
+        child: Text('Sign In'),
+        onPressed: () {},
+      ),
+      SizedBox(height: 8.0),
+      FlatButton(
+        onPressed: (){},
+        child: Text('Need an account Register'),
       ),
     ];
   }
@@ -24,9 +33,12 @@ class EmailSignInForm extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: _buildChildren(),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisSize: MainAxisSize.min,
+          children: _buildChildren(),
+        ),
       ),
     );
   }
