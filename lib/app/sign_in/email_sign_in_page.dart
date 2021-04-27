3d7669e0d7f7f41166a592b431e0f81ace8773ba
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-04-27 13:34:13 +0600

    186,187 Adding a Card widget & Adding the email and password text fields
    186,187 Adding a Card widget & Adding the email and password text fields

--- lib/app/sign_in/email_sign_in_page.dart
+++ lib/app/sign_in/email_sign_in_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:time_tracker_app/app/sign_in/email_sign_in_form.dart';
+// import 'package:time_tracker_flutter_course/app/sign_in/email_sign_in_form.dart';
 
 class EmailSignInPage extends StatelessWidget {
   @override
@@ -8,12 +10,13 @@ class EmailSignInPage extends StatelessWidget {
         title: Text('Sign in'),
         elevation: 2.0,
       ),
-      body: _buildContent(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Card(
+          child: EmailSignInForm(),
+        ),
+      ),
       backgroundColor: Colors.grey[200],
     );
-  }
-
-  Widget _buildContent() {
-    return Container();
   }
 }
