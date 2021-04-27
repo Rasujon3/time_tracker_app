31f3b97872055e8c53c961e439950c5485d6da22
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-04-27 14:27:35 +0600

    190 Adding a TextEditingController
    190 Adding a TextEditingController

--- lib/app/sign_in/email_sign_in_form.dart
+++ lib/app/sign_in/email_sign_in_form.dart
@@ -2,25 +2,43 @@ import 'package:flutter/material.dart';
 import 'package:time_tracker_app/common_widgets/form_submit_button.dart';
 
 class EmailSignInForm extends StatelessWidget {
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  // String email;
+  // String password;
+
+  void _submit() {
+    // TODO: print email and password
+    // print(email);
+    // print(password);
+    print('Email: ${_emailController.text}, password: ${_passwordController.text}');
+
+  }
+
   List<Widget> _buildChildren() {
     return [
       TextField(
+        controller: _emailController,
         decoration: InputDecoration(
           labelText: 'Email',
           hintText: 'test@test.com',
         ),
+        // onChanged: (value) => email = value,
       ),
       SizedBox(height: 8.0),
       TextField(
+        controller: _passwordController,
         decoration: InputDecoration(
           labelText: 'Password',
         ),
         obscureText: true,
+        // onChanged: (value) => password = value,
       ),
       SizedBox(height: 8.0),
       FormSubmitButton(
          text: 'Sign In',
-        onPressed: () {},
+        onPressed: _submit,
       ),
       SizedBox(height: 8.0),
       FlatButton(
